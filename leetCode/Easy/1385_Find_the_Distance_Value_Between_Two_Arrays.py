af9ea5c8b1cbcceac6843981e9228a4a747b3fb2
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2022-04-06 22:39:55 +0900

    Create 1385_Find_the_Distance_Value_Between_Two_Arrays.py

---
+++ leetCode/Easy/1385_Find_the_Distance_Value_Between_Two_Arrays.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        ans = len(arr1)
+        
+        for i in range(len(arr1)):
+            for j in range(len(arr2)):
+                if abs(arr1[i] - arr2[j]) <= d :
+                    ans -= 1
+                    break
+        
+        return ans
